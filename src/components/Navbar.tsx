fd5005a3d227f38107a4be40080e1b2c0c84613c
Cristian <criricyt@gmail.com>
2024-02-21 10:17:39 -0300

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+export function Navbar(){
+    return(
+        <nav className="py-6 px-16 bg-slate-200">
+            <ul className="flex gap-5">
+                <li>Sobre</li>
+                <li>Skills</li>
+                <li>Projetos</li>
+                <li>Contato</li>
+            </ul>
+        </nav>
+    )
+}
